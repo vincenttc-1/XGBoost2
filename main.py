666837cd4d82202862841500f1bd0bebb1e833ac
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-17 19:53:42 +0700

    update

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ import pandas as pd
 import numpy as np 
 #import matplotlib.pyplot as plt
 #import seaborn as sns
-#import re 
-#import nltk
+import re 
+import nltk
 import string
 import json
 
-#nltk.download('punkt')
+nltk.download('punkt')
 
 # import StemmerFactory class
 #from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
