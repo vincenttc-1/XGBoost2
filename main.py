88ca3202e5a7f37440f389d2c321f42bb7c640f2
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-17 18:34:21 +0700

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Original file is located at
 
 # Commented out IPython magic to ensure Python compatibility.
 import pandas as pd
-import numpy as np
+import numpy as np 
 #import matplotlib.pyplot as plt
 import seaborn as sns
 import re
