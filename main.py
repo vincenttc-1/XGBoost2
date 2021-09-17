8095ea677290c7c18f7ad9d1a5f07e8dd091f69d
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-17 18:52:15 +0700

    update

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ Original file is located at
 import pandas as pd
 import numpy as np 
 #import matplotlib.pyplot as plt
-import seaborn as sns
-import re
-import nltk
+#import seaborn as sns
+#import re
+#import nltk
 import string
 import json
 
-nltk.download('punkt')
+#nltk.download('punkt')
 
 # import StemmerFactory class
 #from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
