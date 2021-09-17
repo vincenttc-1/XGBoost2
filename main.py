825aa8114d8998e123e4fd15106fd4d879fa8e7e
vincenttc-1 <vincentcengtjendra@gmail.com>
2021-09-17 20:40:54 +0700

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1iAyiAvlGVEHp_z9Hxbyxm-0LG4GwUSpc"""
 
 # Commented out IPython magic to ensure Python compatibility.
+import pandas as pd
 import re
 import string
 from flask import Flask,jsonify,request
